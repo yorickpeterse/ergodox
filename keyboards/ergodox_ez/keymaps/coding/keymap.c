b995cc8e3ca77eb7ebea1433beee9e1e21736dd0
Yorick Peterse <yorick@yorickpeterse.com>
2020-08-22 15:24:10 +0200

    Update coding layout

--- keyboards/ergodox_ez/keymaps/coding/keymap.c
+++ keyboards/ergodox_ez/keymaps/coding/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_J,           KC_L,           KC_U,           KC_Y,           LCTL(KC_LBRACKET),KC_DELETE,
     KC_LCTRL,       KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                                                           KC_K,           KC_N,           KC_E,           KC_I,           KC_O,           KC_BSPACE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_TRANSPARENT,                                 KC_RALT,        MO(1),          KC_M,           KC_H,           LCTL(KC_RBRACKET),KC_UP,          KC_LALT,
-    LCTL(KC_F1),    LCTL(KC_F2),    LCTL(KC_F3),    LCTL(KC_F4),    KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_RIGHT,
+    LCTL(KC_F1),    LCTL(KC_F2),    LCTL(KC_F3),    LCTL(KC_F4),    KC_CAPSLOCK,                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_RIGHT,
                                                                                                     KC_LGUI,        LALT(KC_F11),   LALT(LCTL(KC_DELETE)),RESET,
                                                                                                                     KC_AUDIO_VOL_UP,KC_PGUP,
                                                                                     KC_SPACE,       KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_PGDOWN,      KC_TRANSPARENT, KC_ENTER
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_ergodox_pretty(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TILD,        KC_GRAVE,       KC_LABK,        KC_BSLASH,      KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_RABK,        KC_CIRC,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-    KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
@@ -75,7 +75,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
+// The state of the caps key, used to toggle the red LED.
+static uint8_t CAPS_PRESSED = 0;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if(keycode == KC_CAPSLOCK) {
+        if(record->event.pressed) {
+            // We only run this when the key is pressed, otherwise releasing the
+            // key would immediately turn the LED off again.
+            if(CAPS_PRESSED == 0) {
+                ergodox_right_led_3_on();
+            } else {
+                ergodox_right_led_3_off();
+            }
+
+            CAPS_PRESSED = !CAPS_PRESSED;
+        }
+    }
+
     return true;
 }
 
@@ -84,8 +101,6 @@ uint32_t layer_state_set_user(uint32_t state) {
 
     ergodox_board_led_off();
     ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
 
     switch (layer) {
         case 3:
